4c5faba9ff38dbe27f4fb2f2ea209db1247874df
Espen Steen <espenhs@gmail.com>
2022-12-25 13:15:58 +0100

    Comment fix

--- files_and_folders.py
+++ files_and_folders.py
@@ -72,7 +72,7 @@ def create_script_folders(directory: str, folders: list[dict], scripts: list[dic
 def create_trigger_files(triggers_directory: str, triggers: list[dict]) -> None:
     for t in triggers:
         description = t.get("description")
-        # Triggers might be unnamed in SuperOffice, which is not allowed in Windows
+        # Triggers might have empty names. Use ID to avoid adding only 1 file.
         if not description:
             description = f"Unnamed trigger (ID {t.get('unique_identifier')})"
 
